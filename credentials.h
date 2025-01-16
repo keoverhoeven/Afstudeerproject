5f2c493d8a03626b44b709c28550ac1e4e3999e1
keoverhoeven <120641457+keoverhoeven@users.noreply.github.com>
2025-01-16 23:27:07 +0100

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,14 @@
+// WiFi-instellingen
+const char* SSID = "";
+const char* PASSWORD = "";
+
+// API-instellingen
+#define SERVERADDRESS ""
+#define PORT 5000
+#define APIENDPOINT "/api/data"
+#define APIENDPOINT2 "/api/oefeningen"
+#define APIENDPOINT3 "/api/receive"
+#define APIKEY ""
+
+#define MQTT_USERNAME "";
+#define MQTT_WACHTWOORD "";
